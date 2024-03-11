f775929393db76fb9a5e2761d4ff0b54018a6e09
cutsea110 <cutsea110@gmail.com>
2024-03-11 18:39:40 +0900

    fix

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 
 export const handler = async(event) => {
     let body = JSON.parse(event.body);
-    const product = body.num1 + body.num2;
+    const product = body.num1 * body.num2;
 
     const s3 = new S3Client({});
     const params = {
