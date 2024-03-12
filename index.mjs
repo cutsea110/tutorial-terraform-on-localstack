900bed01c3f8da19662d0226e791d17ffec4c71c
cutsea110 <cutsea110@gmail.com>
2024-03-12 09:49:32 +0900

    fix: lambda

--- index.mjs
+++ index.mjs
@@ -1,19 +1,26 @@
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 
 export const handler = async(event) => {
-    let body = JSON.parse(event.body);
-    const product = body.num1 * body.num2;
+    let records = event.Records.map(function(record) {
+      let body = Buffer.from(record.kinesis.data, 'base64').toString('ascii');
+      let rec  = JSON.parse(body);
+      return { "num1": rec.body.num1
+             , "num2": rec.body.num2
+             , "product": rec.body.num1 * rec.body.num2
+             , "raw": record.kinesis.data
+             };
+    });
 
     const s3 = new S3Client({});
     const params = {
 	Bucket: "local-archive",
 	Key: "test" + Date.now() + ".json",
-	Body: JSON.stringify({ product }),
+	Body: JSON.stringify({ records }),
     };
     await s3.send(new PutObjectCommand(params));
 
     return {
 	statusCode: 200,
-	body: JSON.stringify({ product }),
+	body: JSON.stringify({ records }),
     };
 };
